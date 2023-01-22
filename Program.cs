36508d52aad2f63aff909e9a65eb47c38e86883e
Сергей <@ANDREYNOTEBOOK>
2023-01-22 18:59:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿AirsoftWeaponStore AWS = new AirsoftWeaponStore();
+Console.WriteLine(AWS);
+class AirsoftWeaponStore
+{
+   private string name;
+    private int price;
+    private int countInStore;
+
+    public string Name { get => name; set => name = value; }
+    public int Price { get => price; set => price = value; }
+    public int CountInStore { get => countInStore; set => countInStore = value; }
+    public AirsoftWeaponStore()
+    {
+        Name = "AK 105";
+        Price = 10000;
+        CountInStore = 120;
+    }
+    public AirsoftWeaponStore(string _name, int _price,int _count)
+    {
+        _name=Name;
+        _price = Price;
+        _count = CountInStore;
+    }
+    public override string ToString() 
+    {
+        return "Name:" + Name + " " + "Price:" + Price + " Rubels " + "Count in Store:" + CountInStore;
+    
+    }
+   
+    
+}
